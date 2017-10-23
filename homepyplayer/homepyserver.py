bd7cede3e61e9b6a312431ed262a9026e518511a
Clément Elvira <clementh.elvira@gmail.com>
2017-10-23 21:13:24 +0200

    New architecture : dedicated module for serveur media

--- homepyplayer/homepyserver.py
+++ homepyplayer/homepyserver.py
@@ -22,7 +22,7 @@ ADDRESS = ''
 PORT = 10000
 RESPONSE_SERVEUR = 'OK'
 END_TRAME = 'FINTRAME'
-
+HOMEPY_SOCKET_TIME_OUT = 2.0
 
 class AtomicBool:
     """An atomic, thread-safe quit flag.
@@ -107,6 +107,11 @@ class HomePyNetwork(object):
         # Create a TCP/IP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        # The socket might raise an exception if the address has been
+        # used recently.
+        # This option tells the socket to reuse the adress
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
         # Bind the socket to the port
         server_address = (ADDRESS, PORT)
         self.sock.bind(server_address)
@@ -129,7 +134,7 @@ class HomePyNetwork(object):
 
                 while not self.atomicBoolQuit.get():
                     try:
-                        connection.settimeout(5.0)
+                        connection.settimeout(HOMEPY_SOCKET_TIME_OUT)
                         recevied_message = ''
                         while True:
                             data = connection.recv(2048)
@@ -180,6 +185,44 @@ class HomePyNetwork(object):
         self.atomicBoolQuit.set(True)
 
 
+class HomePyMedia(object):
+    """docstring for HomePyMedia"""
+
+    def __init__(self, callbackRadioStop):
+        super(HomePyMedia, self).__init__()
+
+        self.vlc_instance = None
+        self.vlc_player = None
+        self.quit = AtomicBool(False)
+
+        self.media_radio = None
+
+        self.callbackRadioStop = callbackRadioStop
+
+    def listen_radio(self, url_radio):
+        self.media_radio = self.vlc_instance.media_new(url_radio)
+        self.vlc_player.set_media(self.media_radio)
+        self.vlc_player.audio_set_volume(100)
+        self.vlc_player.event_manager().event_attach(vlc.EventType.MediaPlayerEndReached, self.callbackRadioStop)
+        self.vlc_player.play()
+
+    def start(self):
+        self.vlc_instance = vlc.Instance()
+        self.vlc_player = self.vlc_instance.media_player_new()
+
+    def play(self):
+        if self.vlc_player is not None:
+            self.vlc_player.play()
+
+    def pause(self):
+        if self.vlc_player is not None:
+            self.vlc_player.pause()
+
+    def set_volume(self, newVolume):
+        if self.vlc_player is not None:
+            self.vlc_player.audio_set_volume(int(newVolume))
+
+
 class HomePyServer(object):
     """
         docstring for HomePyServer
@@ -189,13 +232,12 @@ class HomePyServer(object):
         super(HomePyServer, self).__init__()
 
         self.moduleNetwork = None
-
-        self.instance = vlc.Instance()
-        self.player = self.instance.media_player_new()
-        self.quit = AtomicBool(False)
+        self.moduleMedia = None
 
         self.queueEvent = queue.Queue()
         self.thread_server = None
+
+        self.atomicBoolQuit = AtomicBool(False)
 
     def __exit__(self):
         print('Byyyy :)')
@@ -227,6 +269,13 @@ class HomePyServer(object):
         except Exception as e:
             raise e
 
+        try:
+            self.moduleMedia = HomePyMedia(self.callbackRadioStop)
+            self.moduleMedia.start()
+            print('\tReady to stream music')
+
+        except Exception as e:
+            raise e
 
         self.whenStarted()
 
@@ -240,7 +289,7 @@ class HomePyServer(object):
         shell = ServerHomePyShell()
         shell.setServer(self)
 
-        while not self.quit.get():
+        while not self.atomicBoolQuit.get():
             try:
                 event = self.queueEvent.get(True, None)
                 shell.onecmd(event)
@@ -249,32 +298,30 @@ class HomePyServer(object):
                 print('The command is not implemented yet')
 
         # The program is about to quit, wait for the thread
+        print('Closing network...')
         self.moduleNetwork.closeNetwork()
         self.thread_server.join()
+        print('done')
 
     def kill(self):
-        self.quit.set(True)
+        self.atomicBoolQuit.set(True)
 
     def changeVolume(self, newvolume):
-
-        self.player.audio_set_volume(int(newvolume))
+        self.moduleMedia.set_volume(newvolume)
 
     def play(self, bool_action):
 
-        if self.player.is_playing() and not bool_action:
-            self.player.pause()
+        if self.moduleMedia is not None and not bool_action:
+            self.moduleMedia.pause()
 
-        elif not self.player.is_playing() and bool_action:
-            self.player.play()
+        elif self.moduleMedia is not None and bool_action:
+            self.moduleMedia.play()
 
     def whenStarted(self):
 
         print('By default, the french radio FIP is streamed')
-        self.media = self.instance.media_new(URL_FIP)
-        self.player.set_media(self.media)
-        self.player.audio_set_volume(100)
-        self.player.event_manager().event_attach(vlc.EventType.MediaPlayerEndReached, self.callbackRadioStop)
-        self.player.play()
+        self.moduleMedia.listen_radio(URL_FIP)
+        self.moduleMedia.set_volume(100)
 
 
 if __name__ == '__main__':
@@ -285,3 +332,5 @@ if __name__ == '__main__':
 
     my_pyserver = HomePyServer()
     my_pyserver.start()
+
+    print('by :)')
