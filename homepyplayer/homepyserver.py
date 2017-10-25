e146b8a97fff19cc9dae79ca6f747951048437dd
Clément Elvira <clementh.elvira@gmail.com>
2017-10-25 09:01:42 +0200

    threading Connection : more than one user can be connected now :)

--- homepyplayer/homepyserver.py
+++ homepyplayer/homepyserver.py
@@ -28,6 +28,7 @@ PORT = 10000
 RESPONSE_SERVEUR = 'OK'
 END_TRAME = 'FINTRAME'
 HOMEPY_SOCKET_TIME_OUT = 2.0
+MAX_CONNECTION = 20
 
 
 class ExceptionDoNotExist(Exception):
@@ -120,6 +121,82 @@ class ServerHomePyShell(AHomePyShell):
         self.sock = None
 
 
+class ClientThread(threading.Thread):
+
+    def __init__(self, client_adress, clientsocket, callbackEvent):
+
+        threading.Thread.__init__(self)
+        self.ip = client_adress[0]
+        self.port = client_adress[1]
+        self.clientsocket = clientsocket
+        self.callbackProcessEvent = callbackEvent
+        self.atomicBoolQuitSocket = AtomicBool(False)
+        self.isRunning = AtomicBool(False)
+
+        print("[+] New thrad for %s %s" % (self.ip, self.port, ))
+
+    def run(self): 
+   
+        self.isRunning.set(True)
+        print(" Listening " + str(self.ip) + ':' + str(self.port))
+
+        try:
+            while not self.atomicBoolQuitSocket.get():
+                try:
+                    self.clientsocket.settimeout(HOMEPY_SOCKET_TIME_OUT)
+                    recevied_message = ''
+                    while True:
+                        data = self.clientsocket.recv(2048)
+                        if data:
+                            recevied_message += data.decode()
+                        else:
+                            break
+
+                        if recevied_message.endswith(END_TRAME):
+                            break
+                        elif len(recevied_message) > 1000:
+                            print('A cmd longer than 1000 character has been received')
+                        break
+
+                    self.clientsocket.sendall(RESPONSE_SERVEUR.encode())
+                    recevied_message = recevied_message.replace(END_TRAME, '')
+
+                    print('received: ' + recevied_message)
+                    self.callbackProcessEvent(recevied_message)
+
+                except socket.timeout as e:
+                    err = e.args[0]
+                    # this next if/else is a bit redundant, but illustrates
+                    # how the timeout exception is setup
+                    if err == 'timed out':
+                        time.sleep(1)
+                        continue
+                    else:
+                        print(e)
+
+                except socket.error as e:
+                    # Something else happened, print error
+                    # and handle it later
+                    print(e)
+                    break
+
+                except Exception as e:
+                    # Something else happened, print error
+                    # and handle it later
+                    print(e)
+                    break
+
+        finally:
+            # Clean up the connection
+            self.clientsocket.close()
+
+        self.isRunning.set(False)
+        print(str(self.ip) + ':' + str(self.port) + ' has deconnected')
+
+    def quit(self):
+        self.atomicBoolQuitSocket.set(True)
+
+
 class HomePyNetwork(object):
 
     def __init__(self, callbackEvent):
@@ -128,6 +205,8 @@ class HomePyNetwork(object):
         self.sock = None
         self.atomicBoolQuit = AtomicBool(False)
         self.callbackProcessEvent = callbackEvent
+
+        self.listSocketClient = []
 
     def bindAddress(self):
 
@@ -149,64 +228,41 @@ class HomePyNetwork(object):
         """
 
         # Listen for incoming connections
+        self.sock.settimeout(0.2)
         self.sock.listen(1)
 
         while not self.atomicBoolQuit.get():
             # Wait for a connection
-            print('waiting for a connection')
-            connection, client_address = self.sock.accept()
 
             try:
-                print('connection from ' + str(client_address))
+                connection, client_address = self.sock.accept()
 
-                while not self.atomicBoolQuit.get():
-                    try:
-                        connection.settimeout(HOMEPY_SOCKET_TIME_OUT)
-                        recevied_message = ''
-                        while True:
-                            data = connection.recv(2048)
-                            if data:
-                                recevied_message += data.decode()
-                            else:
-                                break
+                newthread = ClientThread(client_address, connection, self.callbackProcessEvent)
+                newthread.start()
 
-                            if recevied_message.endswith(END_TRAME):
-                                break
-                            elif len(recevied_message) > 1000:
-                                print('A cmd longer than 1000 character has been received')
-                                break
+            except socket.timeout:
+                pass
+            
+            except:
+                raise
+            else:
+                self.listSocketClient.append(newthread)
 
-                        connection.sendall(RESPONSE_SERVEUR.encode())
-                        recevied_message = recevied_message.replace(END_TRAME, '')
+            self.listSocketClient = [t for t in self.listSocketClient if t.is_alive()]
+            thread_to_kill = [t for t in self.listSocketClient if not t.is_alive()]
 
-                        print('received: ' + recevied_message)
-                        self.callbackProcessEvent(recevied_message)
+            for t in thread_to_kill:
+                print('before join')
+                t.join()
+                print('thread t has joined in loop')
 
-                    except socket.timeout as e:
-                        err = e.args[0]
-                        # this next if/else is a bit redundant, but illustrates
-                        # how the timeout exception is setup
-                        if err == 'timed out':
-                            time.sleep(1)
-                            continue
-                        else:
-                            print(e)
-
-                    except socket.error as e:
-                        # Something else happened, print error
-                        # and handle it later
-                        print(e)
-                        break
-
-                    except Exception as e:
-                        # Something else happened, print error
-                        # and handle it later
-                        print(e)
-                        break
-
-            finally:
-                # Clean up the connection
-                connection.close()
+        # Then kill all socket thread
+        print('End loop')
+        for t in self.listSocketClient:
+            t.quit()
+            print('thread t has quit')
+            t.join()
+            print('thread t has joined')
 
     def closeNetwork(self):
         self.atomicBoolQuit.set(True)
